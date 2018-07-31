1a22bbe49b06a2fd4b584950eebde17d0f2b84fc
baralAnurag <ab7324@truman.edu>
2018-07-30 22:43:36 -0400

    deleted a function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,25 +10,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
-  public type1 =["1001","2002","3003","4004","5005"];
-  public types = ["1001","2002","3003"];
-  public myArray: string[] = [];
-  
-  public getMatch(){
-    let index = 0;
-    for(var i = 0; i < this.type1.length; i++){
-      var isMatch = false;
-      for(var j = 0; j < this.types.length; j++){
-        if(this.type1[i]){
-        isMatch = true;
-        this.myArray.push("1001");
-        break;
-      }
-    }
-    if(!isMatch){
-      this.myArray.push("2000");
-    }
-  }
-  console.log(this.myArray);
-  }
 }
