0ba8eb95780c45ff5a1b65943573de6b6cec03b7
Anurag Baral <ab7324@truman.edu>
2018-08-01 22:31:31 -0400

    Building product list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   template:`
-  <div><h1>{{pageTitle}}</h1>
-    <div> My first Component </div>
-    <div *ngFor="let item of myArray"> {{ item }} </div>
-    </div>`
+  <div>
+    <h1>{{pageTitle}}</h1>
+  </div>
+  `
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
